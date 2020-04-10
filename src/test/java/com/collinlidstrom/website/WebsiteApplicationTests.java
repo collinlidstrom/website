a36038e51b8cfc1f4e0fe8052c3f66ebbad69160
Collin Lidstrom <collinlidstrom@gmail.com>
2020-04-10 06:08:32 -0500

    Initial commit.

---
+++ src/test/java/com/collinlidstrom/website/WebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collinlidstrom.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
